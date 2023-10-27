f44c9aa1e7ce11375394298a7c5e4b9dfd395565
Rabih-Nadr <rabih.nadr@gmail.com>
2023-10-26 22:24:46 -0400

    home content

--- duko-transport/src/components/Home.js
+++ duko-transport/src/components/Home.js
@@ -8,9 +8,15 @@ function Home() {
     <section id='home'>
     <div className="home">
       <div className="home-content">
+        <h1>
+        Your Reliable Shipping Partner
+        </h1>
         <p>
-          Welcome to our website! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget felis ut odio
-          sollicitudin elementum. Suspendisse potenti. 
+        Welcome to Duko Transport Inc, your trusted shipping solution right here in Ontario, Canada.
+        We specialize in providing efficient and secure shipping services tailored to your local needs.
+        Whether it's moving freight, managing logistics, or ensuring timely deliveries within Ontario, we're here to make it happen.
+        With a strong commitment to reliability and customer satisfaction, Duko Transport Inc is your go-to partner for seamless transportation solutions.
+       <br/> Let's simplify your shipping experience.
         </p>
       </div>
     </div>
